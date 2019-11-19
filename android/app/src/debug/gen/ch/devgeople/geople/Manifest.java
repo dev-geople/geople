c645fab13774275219b7a5bcd8e394399401a169
Patrick Bauersfeld <patrick.bauersfeld@gmx.ch>
2019-11-19 14:57:38 +0100

    created branch

---
+++ android/app/src/debug/gen/ch/devgeople/geople/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ch.devgeople.geople;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
